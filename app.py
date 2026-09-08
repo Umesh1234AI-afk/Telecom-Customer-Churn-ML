f3fda1aee7f1cc6bf1985bfd38eefe79ff8e22f1
Umesh Chandra <chandraumesh796@gmail.com>
2026-09-08 11:55:43 +0530

    Initial telecom churn ML deployment

---
+++ app.py
@@ -0,0 +1,127 @@
+import streamlit as st
+import joblib
+
+model = joblib.load("churn_model.pkl")
+
+st.title("Telecom Customer Churn Prediction")
+st.write("Enter customer details to predict whether the customer may churn.")
+gender = st.selectbox(
+    "Gender",
+    ["Male", "Female"]
+)
+senior_citizen = st.selectbox("Senior Citizen", [0, 1])
+partner = st.selectbox("Partner", ["Yes", "No"])
+dependents = st.selectbox("Dependents", ["Yes", "No"])
+
+tenure = st.number_input(
+    "Tenure (Months)",
+    min_value=0,
+    max_value=100,
+    value=1
+)
+
+phone_service = st.selectbox(
+    "Phone Service",
+    ["Yes", "No"]
+)
+
+multiple_lines = st.selectbox(
+    "Multiple Lines",
+    ["No", "Yes", "No phone service"]
+)
+
+internet_service = st.selectbox(
+    "Internet Service",
+    ["DSL", "Fiber optic", "No"]
+)
+online_security = st.selectbox(
+    "Online Security",
+    ["Yes", "No", "No internet service"]
+)
+
+online_backup = st.selectbox(
+    "Online Backup",
+    ["Yes", "No", "No internet service"]
+)
+
+device_protection = st.selectbox(
+    "Device Protection",
+    ["Yes", "No", "No internet service"]
+)
+
+tech_support = st.selectbox(
+    "Tech Support",
+    ["Yes", "No", "No internet service"]
+)
+
+streaming_tv = st.selectbox(
+    "Streaming TV",
+    ["Yes", "No", "No internet service"]
+)
+
+streaming_movies = st.selectbox(
+    "Streaming Movies",
+    ["Yes", "No", "No internet service"]
+)
+
+contract = st.selectbox(
+    "Contract",
+    ["Month-to-month", "One year", "Two year"]
+)
+
+paperless_billing = st.selectbox(
+    "Paperless Billing",
+    ["Yes", "No"]
+)
+
+payment_method = st.selectbox(
+    "Payment Method",
+    [
+        "Electronic check",
+        "Mailed check",
+        "Bank transfer (automatic)",
+        "Credit card (automatic)"
+    ]
+)
+
+monthly_charges = st.number_input(
+    "Monthly Charges",
+    min_value=0.0,
+    value=20.0
+)
+
+total_charges = st.number_input(
+    "Total Charges",
+    min_value=0.0,
+    value=0.0
+)
+import pandas as pd
+
+input_data = pd.DataFrame({
+    "gender": [gender],
+    "SeniorCitizen": [senior_citizen],
+    "Partner": [partner],
+    "Dependents": [dependents],
+    "tenure": [tenure],
+    "PhoneService": [phone_service],
+    "MultipleLines": [multiple_lines],
+    "InternetService": [internet_service],
+    "OnlineSecurity": [online_security],
+    "OnlineBackup": [online_backup],
+    "DeviceProtection": [device_protection],
+    "TechSupport": [tech_support],
+    "StreamingTV": [streaming_tv],
+    "StreamingMovies": [streaming_movies],
+    "Contract": [contract],
+    "PaperlessBilling": [paperless_billing],
+    "PaymentMethod": [payment_method],
+    "MonthlyCharges": [monthly_charges],
+    "TotalCharges": [total_charges]
+})
+
+if st.button("Predict Churn"):
+    prediction = model.predict(input_data)[0]
+    probability = model.predict_proba(input_data)[0][1]
+
+    st.write("Prediction:", prediction)
+    st.write("Churn Probability:", round(probability * 100, 2), "%")
